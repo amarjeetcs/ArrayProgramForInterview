8b1a700215b899176421521c315fb15a68f4d20d
Amarjeet Kumar Singh <53248566+amarjeetcs@users.noreply.github.com>
2022-02-19 12:37:11 +0530

    Update FindDuplicate.java
    Latest Code

--- src/FindDuplicate.java
+++ src/FindDuplicate.java
@@ -1,26 +1,15 @@
-
 public class FindDuplicate {
-
-	public static void main(String[] args) {
-		//String[] arr= {"java","c","c++","python","java"};
-		int []arr= {1,2,2,3,4,5,5};
-		boolean flag=false;
-		for(int i=0;i<arr.length;i++)
+public static void main(String[] args) {
+	int[] arr= {1,1,22,23,43,43,34,32};
+	for(int i=0;i<arr.length;i++)
+	{
+		for(int j=i+1;j<arr.length;j++)
 		{
-			for(int j=i+1;j<arr.length;j++)
+			if(arr[i]==arr[j])
 			{
-				if(arr[i]==arr[j])
-				{
-					System.out.println("found duplicate element:"+arr[i]);
-					flag=true;
-				}
+				System.out.println("duplicate element is:"+arr[j]);
 			}
 		}
-		
-		if(flag==false)
-		{
-			System.out.println("duplicate element not found");
-		}
 	}
-
+}
 }
