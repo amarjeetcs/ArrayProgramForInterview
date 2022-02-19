f58ac718b24ff9c1fd7e1d38ddadb8d971fa4d70
Amarjeet Kumar Singh <53248566+amarjeetcs@users.noreply.github.com>
2022-02-19 15:16:47 +0530

    Update FindOddEvenInArray.java

--- src/FindOddEvenInArray.java
+++ src/FindOddEvenInArray.java
@@ -1,43 +1,27 @@
-/*
-public class FindOddEvenInArray {
-		public static void main(String[] args) {
-			int[] arr= {1,2,3,4,5};
-			System.out.println("even numbers:");
-			for(int i=0;i<arr.length;i++)
-			{
-				if(arr[i]%2==0)
-					System.out.println("number is even"+arr[i]);
-			}
-			System.out.println("odd numbers:");
-			for(int i=0;i<arr.length;i++)
-			{
-				if(arr[i]%2!=0)
-					System.out.println("number is odd"+arr[i]);
-			}
-			
+public class OddEvenInArray {
+public static void main(String[] args) {
+	int[] arr= {1,2,3,4,5};
+	int even=0;
+	int odd=0;
+	System.out.println("Even Numbers");
+	for(int i=0;i<arr.length;i++)
+	{
+		if(arr[i]%2==0)
+		{
+			even++;
+			System.out.println(arr[i]);
 		}
 	}
-*/
-
-class FindOddEvenInArray
-{
-	public static void main(String[] args) {
-		int []arr= {1,2,3,4,5};
-		System.out.println("Even elements");
-		for(int i=0;i<arr.length;i++)
+	System.out.println("Odd Numbers");
+	for(int i=0;i<arr.length;i++)
+	{
+		if(arr[i]%2!=0)
 		{
-			if(arr[i]%2==0)
-			{
-				System.out.println("even elements: "+arr[i]);
-			}
-		}
-		System.out.println("Odd Elements");
-		for(int i=0;i<arr.length;i++)
-		{
-			if(arr[i]%2!=0)
-			{
-				System.out.println("odd elements: "+arr[i]);
-			}
+			odd++;
+			System.out.println(arr[i]);
 		}
 	}
+	System.out.println("Total number of even numbers="+even);
+	System.out.println("Total number of odd numbers="+odd);
+}
 }
